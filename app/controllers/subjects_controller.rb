4932a19945b6ccee53490892577a001fbf5ee37c
ma321b <aa.muhammad1030@gmail.com>
2020-11-22 19:08:35 +0000

    Complete the "Read" operation of CRUD (Displaying all the data)

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,10 +1,12 @@
 class SubjectsController < ApplicationController
   def index
   #  index is used to list all subjects
-  @subjects = Subject.order('position ASC')  # all Subject records in db in ASC order by their position
+    @subjects = Subject.order('position ASC')  # all Subject records in db in ASC order by their position
   end
 
   def show
+    # show is used to display detailed information about a single record shown by index
+    @subject = Subject.find(params[:id]) # find the record with this id and store in instance variable @subject
   end
 
   def new
