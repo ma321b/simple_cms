9d8403beb4fbdcfd0e7dab8db0ef5ca8345c68b2
ma321b <aa.muhammad1030@gmail.com>
2020-11-24 10:10:53 +0000

    Complete the "Create" operation of CRUD (Can now add subject record to db)

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,7 @@
 class SubjectsController < ApplicationController
   def index
   #  index is used to list all subjects
-    @subjects = Subject.order('position ASC')  # all Subject records in db in ASC order by their position
+    @subjects = Subject.order('position ASC') # all Subject records in db in ASC order by their position
   end
 
   def show
@@ -10,9 +10,21 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    # NEW is used to display a new form to put data in to be saved to the db in Subject table
+    @subject = Subject.new # the new subject that we will be creating from the form data
   end
 
   def create
+    # CREATE operation -> handle form processing to add the new record to db
+    @subject = Subject.new(subject_params) # instantiate new subject to be added
+    if @subject.save
+      # if record saved successfully, redirect to index
+      redirect_to(subjects_path)
+    else
+      # otherwise render the form again to correct mistakes in it
+      # good thing is previous values will be saved
+      render('new')
+    end
   end
 
   def edit
@@ -25,6 +37,13 @@ class SubjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def subject_params
+    # this will sanitise the params, allowing only name, visible and position
+    # values to be passed in from params for mass assignment
+    return params.require(:subject).permit(:name, :visible, :position)
   end
 
 end
